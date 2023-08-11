3d6d3cb818335172ad32e6c99b76c5ca45dfb46d
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2023-08-11 21:22:52 +0530

    resolved

--- token-contract/scripts/run.js
+++ token-contract/scripts/run.js
@@ -1,19 +1,19 @@
 const main = async () => {
-    const coinContractFactory = await hre.ethers.getContractFactory('Coins');
-    const coinContract = await coinContractFactory.deploy();
-    await coinContract.deployed();
-
-    console.log("Contract deployed to:", coinContract.address);
-};
+  const coinContractFactory = await hre.ethers.getContractFactory('Coins');
+  const coinContract = await coinContractFactory.deploy();
+  await coinContract.deployed();
   
+  console.log("Contract deployed to:", coinContract.address);
+};
+
 const runMain = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  try {
+      await main();
+      process.exit(0);
+  } catch (error) {
+      console.log(error);
+      process.exit(1);
+  }
 };
 
 runMain();
