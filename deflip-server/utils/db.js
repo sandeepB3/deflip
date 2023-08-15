f38826e4db2f0228ede478147e9bdaed20e422e5
Laukik <laukikpatade22@gmail.com>
2023-08-16 03:43:52 +0530

    changes

--- deflip-server/utils/db.js
+++ deflip-server/utils/db.js
@@ -1,5 +1,9 @@
 import mysql from "mysql";
+import session from "express-session";
+import MySQLStore from "express-mysql-session";
 import { promisify } from "util";
+
+const MySQLStoreSession = MySQLStore(session);
 
 export const db = mysql.createConnection({
     host: "database-1.cfs3lkdpwrvj.ap-southeast-2.rds.amazonaws.com",
@@ -8,6 +12,19 @@ export const db = mysql.createConnection({
     password: "hello1234",
     port: "3306"
 });
+
+export const sessionStore = new MySQLStoreSession({
+    expiration: 1000000000,
+    createDatabaseTable: true,
+    schema:{
+        tableName: 'sessiontbl',
+        columnNames:{
+            session_id: 'sessionID',
+            expires: 'expires',
+            data: 'data'
+        }
+    }
+}, db);
 
 export const connectToDatabase = async () => {
     try {
