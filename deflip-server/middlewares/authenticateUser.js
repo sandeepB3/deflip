3ef8163761db262fecb9e7b8b27c4aac7e73e58b
sandeep <107111616+sandeepB3@users.noreply.github.com>
2023-08-15 10:45:31 +0530

    Update authenticateUser.js

--- deflip-server/middlewares/authenticateUser.js
+++ deflip-server/middlewares/authenticateUser.js
@@ -1,10 +1,9 @@
-export const authenticateUser=(req,res,next)=>{
-    
+export const authenticateUser = (req, res, next) => {
     if(!req.session.user){
-    return res.status(500).send({
-        message:"Your session has been timed out"
-    })
-}
-next()
+        return res.status(500).send({
+            message:"Your session has been timed out"
+        })
+    }
+    next()
 }
 
