71a146fafb5cf6a8131010a8f60c8e4b07deb0ad
sandeep <107111616+sandeepB3@users.noreply.github.com>
2023-08-15 10:41:53 +0530

    Update product.js

--- deflip-server/routes/product.js
+++ deflip-server/routes/product.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import multer from 'multer'
-import { addProduct,addImage,getAllProducts} from '../controllers/product.con.js'
-import { authenticateSupplier } from '../middlewares/authenticateSupplier.js';
+import { addProduct, addImage, getAllProducts} from '../controllers/product.con.js'
+import { authenticateSupplier } from '../middlewares/authenticateSupplier.js'
 
 const router = express.Router({ mergeParams:true })
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-//protected routes postman pe nai test kar paraha
-// router.post('/add',authenticateSupplier, addProduct)
-// router.post('/addImage/:productID',authenticateSupplier, upload.single('image'),addImage)
-router.post('/add',addProduct)
-router.get('/allProducts/:supplierID',getAllProducts)
-router.post('/addImage/:productID',upload.single('image'),addImage)
+
+// Protected Routes
+// router.post('/add', authenticateSupplier, addProduct)
+// router.post('/addImage/:productID', authenticateSupplier, upload.single('image'),addImage)
+
+router.post('/add', addProduct)
+router.get('/allProducts/:supplierID', getAllProducts)
+router.post('/addImage/:productID', upload.single('image'),addImage)
+
 export default router;
