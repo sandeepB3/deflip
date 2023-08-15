6ad9f5b4288342ef5471ba68adbf5b03bea0ab36
sandeep <107111616+sandeepB3@users.noreply.github.com>
2023-08-15 11:27:58 +0530

    Update supplier.con.js

--- deflip-server/controllers/supplier.con.js
+++ deflip-server/controllers/supplier.con.js
@@ -22,132 +22,178 @@ export const addSupplier = async (req, res, next) => {
         res.status(500).send('Internal server error');
     }
 };
+
+
 export const loginSupplier = async (req, res, next) => {
+  try {
+    const { supplierName, password } = req.body;
 
-    try {
-        const { supplierName, password } = req.body;
-
-        db.query(`SELECT * FROM SUPPLIER WHERE supplierName=?`, [supplierName], async (err, result1) => {
-        if (err) {
-            console.error(err);
-            res.status(400).send({
-                code: 400,
-                failed: 'error occurred',
-                error: err,
-            });
-        } else {
-            if (result1 && result1[0]) {
-                
-                const comparison = await bcrypt.compare(password, result1[0].password);
-                if (comparison) {
-                    req.session.supplier=result1[0]
-                   
-                    console.log(req.session)
-                    db.query(`SELECT * FROM PRODUCT WHERE supplierId = ?`,[result1[0].supplierID], async (err, result2) => {
-                        if(err){
-                            console.error(err);
-                            res.status(400).send({
-                                code: 400,
-                                failed: 'error occurred',
-                                error: err,
-                            });
-                        }else{
-                            if(result2){
-                                res.send({
-                                    status_code:200,
-                                    message:"Data Returned",
-                                    supplier: result1[0],
-                                    products:result2
-                                })
-                            }
-                        }
-                    })
-                } else {
-                    res.send({
-                        message: 'Auth Decline',
-                        status_code: 401,
-                        supplier: {},
-                    });
-                }
-            }
-        }
-        });
-    } catch (err) {
+    db.query(`SELECT * FROM SUPPLIER WHERE supplierName=?`, [supplierName], async (err, result1) => {
+      if (err) {
         console.error(err);
-        res.status(500).send('Internal server error');
-    }
+        return res.status(400).send({
+          status_code: 400,
+          message: 'An error occurred',
+          error: err,
+        });
+      }
+
+      if (result1 && result1[0]) {
+        const comparison = await bcrypt.compare(password, result1[0].password);
+        if (comparison) {
+          req.session.supplier = result1[0];
+          console.log(req.session);
+
+          db.query(`SELECT * FROM PRODUCT WHERE supplierId = ?`, [result1[0].supplierID], async (err, result2) => {
+            if (err) {
+              console.error(err);
+              return res.status(400).send({
+                status_code: 400,
+                message: 'An error occurred',
+                error: err,
+              });
+            }
+
+            if (result2) {
+              res.send({
+                status_code: 200,
+                message: 'Data Returned',
+                supplier: result1[0],
+                products: result2,
+              });
+            }
+          });
+        } else {
+          res.send({
+            message: 'Authentication Declined',
+            status_code: 401,
+            supplier: {},
+          });
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status_code: 500,
+      message: 'Internal server error',
+      error: err,
+    });
+  }
 };
 
-// export const getProducts  =async (req, res, next) => {
-  
-//     console.log(req.session.supplier)
-//     try{
-//         const supplierID  = req.body?.supplierID;
-//         db.query(`SELECT * FROM PRODUCT WHERE supplierId = ?`,[supplierID], async (err, result) => {
-//             if(err){
-//                 console.error(err);
-//                 res.status(400).send({
-//                     code: 400,
-//                     failed: 'error occurred',
-//                     error: err,
-//                 });
-//             }else{
-//                 if(result && result[0]){
-//                     console.log(result);
-//                     res.send({
-//                         status_code:200,
-//                         message:"Data Returned"
-//                     })
-//                 }
-//             }
-//         })
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.status(500).send('Internal server error');
-//     }
+
+// export const getProducts = async (req, res, next) => {
+//   try {
+//     console.log(req.session.supplier);
+//     const supplierID = req.body?.supplierID;
+
+//     db.query(`SELECT * FROM PRODUCT WHERE supplierId = ?`, [supplierID], async (err, result) => {
+//       if (err) {
+//         console.error(err);
+//         return res.status(400).send({
+//           status_code: 400,
+//           message: 'An error occurred',
+//           error: err,
+//         });
+//       } else {
+//         if (result && result.length > 0) {
+//           console.log(result);
+//           res.send({
+//             status_code: 200,
+//             message: 'Data Returned',
+//             products: result,
+//           });
+//         } else {
+//           res.send({
+//             status_code: 404,
+//             message: 'No products found',
+//           });
+//         }
+//       }
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send({
+//       status_code: 500,
+//       message: 'Internal server error',
+//       error: err,
+//     });
+//   }
 // };
 
-export const logoutSupplier = async (req, res, next) => {
 
-    try {
-        req.session.supplier=null
-        res.status(201).send({
-            message:"Logged out successfully"
-        })
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal server error');
-    }
+export const logoutSupplier = async (req, res, next) => {
+  try {
+    req.session.supplier = null;
+    res.status(201).send({
+      message: "Logged out successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status_code: 500,
+      message: "Internal server error",
+      error: err,
+    });
+  }
 };
-export const getTopCustomers=async(req,res)=>{
-    const {supplierID}=req.params
-    console.log(supplierID)
-    db.query( `SELECT U.userID,U.emailID, SUM(P.quantity) AS total_quantity_bought
-    FROM USERS U
-    JOIN PURCHASE P ON U.userID = P.userID
-    JOIN PRODUCT PR ON P.productID = PR.productID
-    JOIN SUPPLIER S ON PR.supplierID = S.supplierID
-    WHERE S.supplierID =?
-    GROUP BY U.userID
-    ORDER BY total_quantity_bought DESC
-    LIMIT 10`,[supplierID],async(err,result)=>{
-        if(result){
-            res.send({
-                result
-                });
-           
-        }
-        else{
-            res.send({
-                code: 400,
-                failed: 'error occurred',
-                error: err,
-            });
-        }
-    })
-}
-export const checkAuth=async(req,res)=>{
-if(req.session.supplier) return res.status(200).send({status:"Authenticated"})
-else return res.status(401).send({status:"Invalid"})
-}
+
+
+export const getTopCustomers = async (req, res) => {
+  try {
+    const { supplierID } = req.params;
+
+    const query = `
+      SELECT U.userID, U.emailID, SUM(P.quantity) AS total_quantity_bought
+      FROM USERS U
+      JOIN PURCHASE P ON U.userID = P.userID
+      JOIN PRODUCT PR ON P.productID = PR.productID
+      JOIN SUPPLIER S ON PR.supplierID = S.supplierID
+      WHERE S.supplierID = ?
+      GROUP BY U.userID
+      ORDER BY total_quantity_bought DESC
+      LIMIT 10
+    `;
+
+    db.query(query, [supplierID], async (err, result) => {
+      if (result) {
+        res.send({
+          result,
+        });
+      } else {
+        console.error(err);
+        res.status(400).send({
+          status_code: 400,
+          message: 'An error occurred',
+          error: err,
+        });
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status_code: 500,
+      message: 'Internal server error',
+      error: err,
+    });
+  }
+};
+
+
+export const checkAuth = async (req, res) => {
+  try {
+    if (req.session.supplier) {
+      return res.status(200).send({ status: "Authenticated" });
+    } else {
+      return res.status(401).send({ status: "Invalid" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status_code: 500,
+      message: "Internal server error",
+      error: err,
+    });
+  }
+};
