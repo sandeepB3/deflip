40277d176090b65bd7ef103cc46651816e19782a
sandeep <107111616+sandeepB3@users.noreply.github.com>
2023-08-16 07:17:47 +0530

    Delete deflip-server directory

--- deflip-server/routes/purchase.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { makePurchase ,purchaseCart} from '../controllers/purchase.con.js';
-const router = express.Router( {mergeParams:true} )
-router.post('/make', makePurchase)
-router.post('/cart', purchaseCart)
-export default router;
