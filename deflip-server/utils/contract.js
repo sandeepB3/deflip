ff6a841d3cec74bd34bb1be1b1a595188906ec5d
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2023-08-18 14:34:07 +0530

    deployed to mumbai

--- deflip-server/utils/contract.js
+++ deflip-server/utils/contract.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import Web3 from 'web3';
-const web3 = new Web3("http://127.0.0.1:8545/"); // const web3 = new Web3(process.env.QuickNode_URL);
+const web3 = new Web3(process.env.QUICKNODE_KEY);
 
 import TokenABI from './contractABI/ChainToken.json' assert { type: "json" };
 import KartABI from './contractABI/ChainKart.json' assert { type: "json" };
@@ -20,8 +20,8 @@ const kartContractAddress = "0x0E08bFBBa5E9E5d2BaD3d0dB65B6dD91CECdc6DB";
 let userContractAddress = ''; //Function to retrive
 let sellerContractAddress = ''; //Function to retrive
 
-const PRIVATE_KEY = "d7d4b519da331fe50a825f883347a5084f915b1d7a524cce93a57f2f7ba74840";
-const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
+const privateKey = `0x${process.env.PRIVATE_KEY}`;
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 web3.eth.accounts.wallet.add(account);
 
 export const tokenInstance = new web3.eth.Contract(tokenContractABI, tokenContractAddress);
